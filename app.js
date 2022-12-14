1253c1ee39915e5ff11c58fe366d94349fe48cc3
gary909 <garywhite61@hotmail.com>
2022-12-14 13:00:28 +0100

    code

---
+++ app.js
@@ -0,0 +1,11 @@
+function remove(string) {
+    //coding and coding....
+    return '';
+}
+
+console.log(remove("Hi!")); // "Hi!"
+console.log(remove("Hi!!!")); // "Hi!"
+console.log(remove("!Hi")); // "Hi!"
+console.log(remove("!Hi!")); // "Hi!"
+console.log(remove("Hi! Hi!")); // "Hi Hi!"
+console.log(remove("Hi!")); // "Hi!"
